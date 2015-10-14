58afecfac43927b6982bd526e73b1d60f44c8b16
Xabier Maeztu <trisketonni@gmail.com>
2015-10-14 16:05:45 +0200

    Changing icon. Adding more fields

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,20 +10,40 @@ angular.module('starter.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-  //$scope.actos = Actos.all();
+  $scope.actos = [];
   Actos.all().success(function(data) {
-    $scope.actos = data;
+    angular.forEach(data, function(value, key) {
+      value.fecha = parseInt(value.fecha) * 1000;
+      $scope.actos.push(value);
+    });
   });
-  $scope.remove = function(acto) {
-    Actos.remove(acto);
-  };
 })
 
-.controller('ActoDetailCtrl', function($scope, $stateParams, Actos) {
+.controller('ActoDetailCtrl', function($scope, $stateParams, Actos, $cordovaLocalNotification) {
   Actos.get($stateParams.actoId).success(function(data) {
-    console.log(data);
     $scope.acto = data[0]; 
+    $scope.acto.fecha = parseInt($scope.acto.fecha) * 1000;
   });
+  $scope.add = function() {
+        var alarmTime = new Date();
+        alarmTime.setMinutes(alarmTime.getMinutes() + 1);
+        $cordovaLocalNotification.schedule({
+            id: "1234",
+            date: alarmTime,
+            message: "This is a message",
+            title: "This is a title",
+            autoCancel: false,
+            sound: null
+        }).then(function () {
+            console.log("The notification has been set");
+        });
+    };
+ 
+    $scope.isScheduled = function() {
+        $cordovaLocalNotification.isScheduled("1234").then(function(isScheduled) {
+            alert("Notification 1234 Scheduled: " + isScheduled);
+        });
+    }
 })
 
 .controller('AccountCtrl', function($scope) {
