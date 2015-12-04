f2f10e9a8654a42915e5c56c8528b7e4d177e75b
Xabier Maeztu <trisketonni@gmail.com>
2015-12-04 17:11:21 +0100

    Adding spinner while loading

--- www/js/services.js
+++ www/js/services.js
@@ -3,12 +3,12 @@ angular.module('starter.services', [])
 .factory('Actos', ['$http', 'Settings', function($http, Settings) {
   return {
     all: function() {
-      //return  $http.get('http://katakrak.net/'+Settings.getLang()+'/api/agenda/views/service_agenda');
-      return  $http.get('api/'+Settings.getLang()+'/apiviews/service_agenda');
+      return  $http.get('http://katakrak.net/'+Settings.getLang()+'/api/agenda/views/service_agenda');
+      return  $http.get('api/'+Settings.getLang()+'/api/views/service_agenda');
     },
     get: function(actoId) { 
-      //return  $http.get('http://katakrak.net/'+Settings.getLang()+'/api/agenda/views/service_agenda?args='+actoId);
-      return  $http.get('api/'+Settings.getLang()+'/apiviews/service_agenda?args='+actoId);
+      return  $http.get('http://katakrak.net/'+Settings.getLang()+'/api/agenda/views/service_agenda?args='+actoId);
+      return  $http.get('api/'+Settings.getLang()+'/api/views/service_agenda?args='+actoId);
     }
   };
 }])
@@ -16,11 +16,11 @@ angular.module('starter.services', [])
 .factory('Libros', ['$http', 'Settings', function($http, Settings) {
   return {
     all: function() {
-      //return  $http.get('http://katakrak.net/'+Settings.getLang()+'/api/agenda/views/service_libros');
+      return  $http.get('http://katakrak.net/'+Settings.getLang()+'/api/agenda/views/service_libros');
       return  $http.get('api/'+Settings.getLang()+'/api/views/service_libros');
     },
     get: function(actoId) { 
-      //return  $http.get('http://katakrak.net/'+Settings.getLang()+'/api/agenda/views/service_libros?args='+actoId);
+      return  $http.get('http://katakrak.net/'+Settings.getLang()+'/api/agenda/views/service_libros?args='+actoId);
       return  $http.get('api/'+Settings.getLang()+'/api/views/service_libros?args='+actoId);
     }
   };
